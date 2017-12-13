c2a7d91ddad35a253a342682d2a3a87f633b7909
connerledbetter <Cledbetter@mail.greenriver.edu>
2017-12-13 00:46:58 -0800

    validate form function and getting rid of webform
    Got rid of the webform because I came to the realization that I think we need PHP to use that, so i got rid of it and just made input boxes and a button that gathers information that is written in the inputs.
    
    Also made  it so that after you hit the button, the values that were given are made into a JSON object.
    
    Now I need to figure out how to put that JSON object into that table and hopefully we are done then.

--- final1/buttonstuff.js
+++ final1/buttonstuff.js
@@ -1,17 +1,58 @@
-$("#submit").click(function() {
+var fullName;
+var subject;
+var location;
+var availability;
+var phoneNumber;
+var myObj;
 
-  var name = document.getElementById("name").value;
-  var availability = document.getElementById("availability").value;
-  var prefLocation = document.getElementById("prefLocation").value;
-  var needsHelp = document.getElementById("needsHelp").value;
-  var pNumber = document.getElementById("pNumber").value;
+function validateForm() {
+    var fullName = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var location = document.getElementById("location").value;
+    var availability = document.getElementById("availability").value;
+    var phoneNumber = document.getElementById("pNumber").value;
 
-  console.log(name);
-    console.log(availability);
-      console.log(prefLocation);
-        console.log(needsHelp);
-          console.log(pNumber);
-});
+    if (fullName == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    if (subject == "") {
+        alert("Subject must be filled out");
+        return false;
+    }
+    if (location == "") {
+        alert("location must be filled out");
+        return false;
+    }
+    if (availability == "") {
+        alert("availability must be filled out");
+        return false;
+    }
+    if (phoneNumber == "") {
+        alert("phone number must be filled out");
+        return false;
+    }
+
+    console.log(fullName);
+
+    var people = {};
+    var tutors = [];
+    people.tutors = tutors;
+
+    var tutors = {
+      "fullName": fullName,
+      "subject": subject,
+      "location": location,
+      "phoneNumber": phoneNumber,
+      "availability": availability
+    }
+
+    var obj = JSON.stringify(tutors);
+    myObj = JSON.parse(obj);
+    console.log(myObj);
+
+  }
+
 
 $("#addTicket").click(function(){
   window.location.href ="webform.html";
